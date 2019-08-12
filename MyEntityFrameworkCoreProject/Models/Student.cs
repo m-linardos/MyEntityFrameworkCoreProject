5f549a3324102d1993e950604f9f2ec371f69191
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-12 16:34:14 -0400

    added something

--- MyEntityFrameworkCoreProject/Models/Student.cs
+++ MyEntityFrameworkCoreProject/Models/Student.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MyEntityFrameworkCoreProject.Models {
     public class Student {
 
         public int Id { get; set; }
-        //[Required]
-        //[StringLength(30)]
+        [Required]
+        [StringLength(30)]
         public string Firstname { get; set; }
-        //[StringLength(30)]
-        //[Required]
+        [StringLength(30)]
+        [Required]
         public string Lastname { get; set; }
         public int SAT { get; set; }
         public double GPA { get; set; }
