f23504a0828175bee951c93de7fb370e341cbff3
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-12 00:04:06 -0400

    updated with notes

---
+++ MyEntityFrameworkCoreProject/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEntityFrameworkCoreProject.Models {
+    public class AppDbContext : DbContext  {            /// inherited from DBContext / Import using Microsoft.EntityFrameworkCore;
+                                                        
+
+        public AppDbContext(DbContextOptions/*<AppDbContext>*/ options) : base(options) {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {          /// use this to configure the context
+
+            var connStr = 
+                "server=localhost\\sqlexpress;database=AppEfDb;trust_connection=true;";
+            builder.UseSqlServer(connStr);
+        }
+        public DbSet<Student> Students { get; set; }        /// define the DbSet property for the each entity ( database table).
+    }
+}
---
+++ MyEntityFrameworkCoreProject/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEntityFrameworkCoreProject.Models {
+    class Student {
+
+        public int Id { get; set; }
+        //[Required]
+        //[StringLength(30)]
+        public string Firstname { get; set; }
+        //[StringLength(30)]
+        //[Required]
+        public string Lastname { get; set; }
+        public int SAT { get; set; }
+        public double GPA { get; set; }
+        public bool IsFulltime { get; set; }
+
+    }
+}
